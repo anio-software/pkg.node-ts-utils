ec77b6d1b4f4da3fc50d038e12e381def63ee86a
eXory2024 <exory@anio.software>
2025-03-08 00:52:59 +0100

    Reset package implementation.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,5 +1,3 @@
-import {generateFactoryFiles} from "@fourtune/realm-js/v0/autogenerate"
-
 export default {
 	realm: {
 		name: "js",
@@ -9,18 +7,5 @@ export default {
 			runtime: "node",
 			external_npm_packages: ["typescript"]
 		}
-	},
-
-	autogenerate: {
-		...generateFactoryFiles({
-			source_file: "src/__getExportsRecursive.mts",
-			export_name: "getExportsRecursive",
-			destination: "src/export"
-		}),
-		...generateFactoryFiles({
-			source_file: "src/__getExports.mts",
-			export_name: "getExports",
-			destination: "src/export"
-		})
 	}
 }
